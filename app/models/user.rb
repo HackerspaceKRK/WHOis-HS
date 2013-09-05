cb505cb2d5a8f86d654bdf1d94804723571f1a60
Łukasz Dubiel <bambucha14@gmail.com>
2013-09-06 01:24:29 +0200

    Dodanie hasła oraz prostego mechanizmu uwierzytelnienia.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,25 @@
+require 'digest'
+
 class User
   include DataMapper::Resource
 
   property :id, Serial
-
-
   property :display_name, String, :length => 100, :required => true
-
   property :login, String, :required => true , :length => 10
-  
+  property :last_login, Date 
+  property :password, String, :length => 64, :writer => :private
 
+  def password=(new_password)
+    super(hash_password(new_password))
+  end
 
+  def authenticate(password)
+    return self.password == hash_password(password)
+  end
+
+  private
+
+  def hash_password(password)
+    Digest::SHA2.hexdigest(password)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,28 @@ describe User do
   it { should respond_to :login }
   it { should validate_presence_of(:login) }
   it { should validate_length_of(:login).max(10) }
+
+  it { should respond_to :id }
+
+
+  it { should respond_to :last_login }
+
+  it { should respond_to :password= }
+
+  it { should respond_to :authenticate }
+
+  let(:user){ User.new }
+
+  it "hash passwords" do
+    password = 'ala ma kota'
+    hash = Digest::SHA2.hexdigest(password)
+    user.password = password
+    user.password.should be == hash
+  end
+
+
+  it "authorized same password as was set" do
+    user.password = 'ala ma kota'
+    user.authenticate('ala ma kota').should be_true
+  end
 end
