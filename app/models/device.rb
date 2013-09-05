4a5a38b196b5ec881b77f4ce92928201903b17d6
Łukasz Dubiel <bambucha14@gmail.com>
2013-09-06 01:48:51 +0200

    Urządzenie wie że ma już swojego właściciela.

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,8 +2,8 @@ class Device
   include DataMapper::Resource
 
   property :id, Serial
-
   property :mac, String, :format => /[0-9A-F]{2}(\:[0-9A-F]{2}){5}/
 
+  belongs_to :user
 end
 
--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -5,4 +5,6 @@ describe Device do
   it { should respond_to(:mac) }
 
   it { should validate_format_of(:mac).with(/[0-9A-F]{2}(\:[0-9A-F]{2}){5}/) }
+
+  it { should belong_to(:user) }
 end
