91307b879d8baf9a3a8ad0a3c2f01d538c7d9657
Łukasz Dubiel <bambucha14@gmail.com>
2013-09-06 00:40:20 +0200

    Dodanie środowiska.
    Podczas testów baza jest w pamięci.

--- app/config.rb
+++ app/config.rb
@@ -4,8 +4,18 @@ module Whois
   def self.root 
     ROOT
   end
+
+  def self.env
+    if !!ENV['RACK_ENV']
+      'developent'
+    end
+    return ENV['RACK_ENV']
+  end
   
   def self.db_url
-    "sqlite://#{Whois.root}/db/database.sqlite"
+    if env == 'production'
+      return "sqlite://#{Whois.root}/db/database.sqlite"
+    end
+    return "sqlite::memory:"
   end
 end
