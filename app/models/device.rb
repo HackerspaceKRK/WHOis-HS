a8af61c0f896a3cc11128113d11d26b54ed56f10
Łukasz Dubiel <bambucha14@gmail.com>
2013-09-06 01:46:46 +0200

    Dodanie modelu urzadzenia dla ludzi.

---
+++ app/models/device.rb
@@ -0,0 +1,9 @@
+class Device
+  include DataMapper::Resource
+
+  property :id, Serial
+
+  property :mac, String, :format => /[0-9A-F]{2}(\:[0-9A-F]{2}){5}/
+
+end
+
---
+++ spec/models/device_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Device do
+  
+  it { should respond_to(:mac) }
+
+  it { should validate_format_of(:mac).with(/[0-9A-F]{2}(\:[0-9A-F]{2}){5}/) }
+end
