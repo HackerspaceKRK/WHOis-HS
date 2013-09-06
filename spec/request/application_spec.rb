7d79c5576b90aaf7b439786c700aac0bf0b6b658
Łukasz Dubiel <bambucha14@gmail.com>
2013-09-06 03:24:13 +0200

    Dodanie testów na obecność linków na stronie.

--- spec/request/application_spec.rb
+++ spec/request/application_spec.rb
@@ -1,8 +1,20 @@
+# -*- coding: utf-8 -*-
 require 'capybara_helper'
 
 describe "Application handle" do
 
-  it "request on /" do
-    visit '/'
+  describe  "request on / and result" do
+    before do
+      visit '/'
+    end
+    
+    it "has link to user panel" do
+      page.has_link?('panel', :href => "/panel").should be_true
+    end
+
+    it "has link to registration page" do
+      page.has_link?('się zarejestrować', :href => "/register").should be_true
+    end
   end
+
 end
